c98119669921bab0180105f407db6a92d781de4e
silasoyibs <silasoyibs@gmail.com>
2023-10-28 18:15:03 +0100

    nav-bar completed

--- js.js
+++ js.js
@@ -1,22 +1,36 @@
 "use strict";
-const tabBar = document.querySelector(".header-bar");
-const tabCloseBar = document.querySelector("header .header-xmark");
-const tabCon = document.querySelector(".header-links-tablet ul");
+const tabBar = document.querySelector(".header__hamburger");
+const menuHeader = document.querySelector(".header");
 
-tabBar.addEventListener('click', toggleMenu);
+// Toggling of the mobile menu
+tabBar.addEventListener("click", toggleMenu);
 // Toggle header navigation menu
 let openMenu = false;
 function toggleMenu() {
-  if(!openMenu){
-    tabCon.style.left = "0";
-    tabBar.style.opacity = "0";
-    tabCloseBar.style.display='block'
-    openMenu=true;
+  if (!openMenu) {
+    menuHeader.classList.add("nav-open");
+    openMenu = true;
+  } else {
+    menuHeader.classList.remove("nav-open");
+    openMenu = false;
   }
-   else{
-    tabCon.style.left = "100%";
-    tabBar.style.opacity = "1";
-    tabCloseBar.style.display='none'
-    openMenu=false;
-   } 
 }
+
+// Intersection observer for the mobile menu to disappear on scroll
+function handleIntersection(entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && openMenu) {
+      // Close the menu when it's open and intersects with the viewport
+      toggleMenu();
+    }
+  });
+}
+
+// Create an Intersection Observer to watch the menuHeader
+const menuObserver = new IntersectionObserver(handleIntersection, {
+  threshold: 1, // Adjust the threshold value as needed
+  root: null, // Set the root to the viewport
+});
+
+// Observe the menuHeader
+menuObserver.observe(menuHeader);
